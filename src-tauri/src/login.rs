82d345f1d61a1308ea1a371cb2a46265d01d6081
Tuomas Rinne <59115775+tumrin@users.noreply.github.com>
2022-12-18 22:05:57 +0200

    Fix settings directory (#13)
    * update deps and fix settings path (#10)
    
    * bump version (#11)

--- src-tauri/src/login.rs
+++ src-tauri/src/login.rs
@@ -13,16 +13,20 @@ pub async fn login(
         api_url: Some(api_url),
     };
 
+    let mut app_state = state.0.lock().await;
+
     let toml =
         toml::to_string(&settings).map_err(|err| tauri::InvokeError::from(err.to_string()))?;
-    fs::write("./settings.toml", toml).map_err(|err| tauri::InvokeError::from(err.to_string()))?;
+
+    // Save settings
+    fs::write(&app_state.settings_dir, toml)
+        .map_err(|err| tauri::InvokeError::from(err.to_string()))?;
 
     let password_entry = keyring::Entry::new(CRATE_NAME, settings.kimai_user.as_ref().unwrap()); // Safe to unwrap since we just made this
     password_entry
         .set_password(&api_key)
         .map_err(|err| tauri::InvokeError::from(err.to_string()))?;
 
-    let mut app_state = state.0.lock().await;
     app_state.settings = Some(settings);
 
     Ok(())
@@ -32,7 +36,10 @@ pub async fn login(
 pub async fn logout(state: tauri::State<'_, ApplicationState>) -> Result<(), tauri::InvokeError> {
     let mut app_state = state.0.lock().await;
     let settings = app_state.settings.as_ref().unwrap();
-    fs::remove_file("./settings.toml").unwrap();
+
+    // Remove settings
+    fs::remove_file(&app_state.settings_dir).unwrap();
+
     let password_entry = keyring::Entry::new(CRATE_NAME, &settings.kimai_user.clone().unwrap());
     password_entry
         .delete_password()
